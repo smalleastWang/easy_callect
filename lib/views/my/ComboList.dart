9bd35fa1e7d881a82f808855277a0773dd743c5a
artdong <1029278668@qq.com>
2024-06-19 08:40:20 +0800

    refactor: 套餐列表

--- lib/views/my/ComboList.dart
+++ lib/views/my/ComboList.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:easy_collect/api/combo.dart';
 import 'package:easy_collect/widgets/List/index.dart';
+import 'package:go_router/go_router.dart';
+import 'package:easy_collect/enums/Route.dart';
 
-class ComboListPage extends ConsumerStatefulWidget {
-  const ComboListPage({super.key});
+class ComboListPage extends StatefulWidget {
+  const ComboListPage({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
-  _ComboListPageState createState() => _ComboListPageState();
+  State<ComboListPage> createState() => _ComboListPageState();
 }
 
-class _ComboListPageState extends ConsumerState<ComboListPage> {
+class _ComboListPageState extends State<ComboListPage> {
+  void _navigateToDetail(combo) {
+    context.push(RouteEnum.comboDetail.path, extra: combo);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('套餐'),
+        backgroundColor: Colors.white,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
@@ -25,20 +29,40 @@ class _ComboListPageState extends ConsumerState<ComboListPage> {
           },
         ),
       ),
-      body: ListWidget<ComboPageFamily>(
-        provider: comboPageProvider,
-        builder: (comboData) {
-          return ListTile(
-            contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            leading: const Icon(Icons.folder, color: Colors.orange),
-            title: Text(comboData['costName']),
-            subtitle: Text('创建时间 ${comboData['createTime'] ?? '未知'}'),
-            trailing: const Icon(Icons.chevron_right),
-            onTap: () {
-              // 处理点击事件
-            },
-          );
-        },
+      backgroundColor: Color(0xFFF1F5F9),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+        child: ListWidget<ComboPageFamily>(
+          provider: comboPageProvider,
+          builder: (comboData) {
+            return Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  child: ListTile(
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    leading: const Icon(Icons.folder, color: Colors.orange),
+                    title: Text(comboData['costName']),
+                    subtitle: Text('创建时间 ${comboData['createTime'] ?? '未知'}'),
+                    trailing: const Icon(Icons.chevron_right),
+                    onTap: () {
+                      _navigateToDetail(comboData);
+                    },
+                  ),
+                ),
+                const Divider(
+                  height: 0.5,
+                  color: Color(0xFFE9E8E8),
+                  indent: 12,
+                  endIndent: 12,
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
