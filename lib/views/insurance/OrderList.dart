cba39ae3a30d2e1acc66781c7b97a912b948ecf2
artdong <1029278668@qq.com>
2024-06-26 00:14:56 +0800

    feat: order list

--- lib/views/insurance/OrderList.dart
+++ lib/views/insurance/OrderList.dart
@@ -1,7 +1,7 @@
 import 'package:easy_collect/api/insurance.dart';
 import 'package:easy_collect/enums/Route.dart';
 import 'package:easy_collect/enums/register.dart';
-import 'package:easy_collect/widgets/List/ListItem.dart';
+import 'package:easy_collect/widgets/List/ListCard.dart';
 import 'package:easy_collect/widgets/List/index.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -38,21 +38,39 @@ class _OrderListPageState extends ConsumerState<OrderListPage> {
       body: ListWidget<OrderListFamily>(
         provider: orderListProvider,
         builder: (data) {
-          return ListItemWidget(
-            rowData: data,
-            columns: [
-              ListColumnModal(label: '组织名称', field: 'orgName'),
-              ListColumnModal(label: '保险合同号', field: 'policyNo'),
-              ListColumnModal(label: '保险项目', field: 'policyContent'),
-              ListColumnModal(label: '保险费', field: 'premium'),
-              ListColumnModal(label: '保险合同生效日期', field: 'effectiveTime'),
-              ListColumnModal(label: '保险合同期满日期', field: 'expiryTime'),
-              ListColumnModal(label: '保单创建时间', field: 'createTime'),
-              ListColumnModal(label: '保单状态', field: 'state', render: (value, record, column) => Text(OrderStatus.getLabel(value))),
-              ListColumnModal(label: '联系人', field: 'person'),
-              ListColumnModal(label: '联系电话', field: 'phone'),
-            ]
-          );
+          return Container(
+          margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.white
+          ),
+          child: Column(
+            children: [
+              ListCardTitle(
+                title: Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(right: 5),
+                      padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 5),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFCCD2E1),
+                        borderRadius: BorderRadius.circular(3)
+                      ),
+                      child: Text(OrderStatus.getLabel(data['state']), style: const TextStyle(color: Colors.white)),
+                    ),
+                    Text(data['policyContent'], style: const TextStyle(fontSize: 16, color: Colors.black, fontWeight: FontWeight.w500)),
+                  ],
+                ),
+              ),
+              ListCardCell(label: '保险合同号', value: data['policyNo']),
+              ListCardCell(label: '保险费', value: data['premium'].toString()),
+              ListCardCell(label: '联系人', value: data['person']),
+              ListCardCell(label: '联系电话', value: data['phone']),
+              ListCardCellTime(label: '保单创建时间：', value: data['createTime'])
+            ],
+          ),
+        );
         },
       )
     );
