2b3ee522b374f4b2fdec7267df986c2df0c800c4
artdong <1029278668@qq.com>
2024-07-21 23:54:10 +0800

    feat: 养殖规模

--- lib/views/supervision/BreedingScale.dart
+++ lib/views/supervision/BreedingScale.dart
@@ -1,6 +1,7 @@
 import 'package:easy_collect/api/monitoring.dart';
 import 'package:easy_collect/enums/route.dart';
 import 'package:easy_collect/models/monitoring/Monitoring.dart';
+import 'package:easy_collect/widgets/AreaSelector/AreaSelector.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -13,64 +14,113 @@ class BreedingData {
   BreedingData(this.category, this.value, this.color);
 }
 
-final mortgageInfoProvider = FutureProvider<Monitoring?>((ref) async {
-  return await MonitoringApi.geScaleInfo({"id": null});
+final mortgageInfoProvider = StateNotifierProvider<MonitoringNotifier, AsyncValue<Monitoring?>>((ref) {
+  return MonitoringNotifier();
 });
 
-class BreedingScalePage extends ConsumerWidget {
+class MonitoringNotifier extends StateNotifier<AsyncValue<Monitoring?>> {
+  MonitoringNotifier() : super(const AsyncValue.loading());
+
+  Future<void> fetchMonitoringData({String? provinceId, String? cityId}) async {
+    try {
+      final id = cityId ?? provinceId;
+      final monitoringData = await MonitoringApi.geScaleInfo({
+        "id": id,
+      });
+      state = AsyncValue.data(monitoringData);
+    } catch (error) {
+      print('error: $error');
+    }
+  }
+}
+
+class BreedingScalePage extends ConsumerStatefulWidget {
   const BreedingScalePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  // ignore: library_private_types_in_public_api
+  _BreedingScalePageState createState() => _BreedingScalePageState();
+}
+
+class _BreedingScalePageState extends ConsumerState<BreedingScalePage> {
+  String? selectedProvince;
+  String? selectedCity;
+  String? selectedDistrict;
+
+  @override
+  void initState() {
+    super.initState();
+    ref.read(mortgageInfoProvider.notifier).fetchMonitoringData();
+  }
+
+  void _onAreaSelected(String? province, String? city) {
+    setState(() {
+      selectedProvince = province;
+      selectedCity = city;
+    });
+
+    ref.read(mortgageInfoProvider.notifier).fetchMonitoringData(
+      provinceId: province,
+      cityId: city,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final AsyncValue<Monitoring?> mortgageInfo = ref.watch(mortgageInfoProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(RouteEnum.breedingScale.title),
       ),
-      body: mortgageInfo.when(
-        data: (mortgageInfo) {
-          if (mortgageInfo == null) {
-            return const Center(child: Text('未获取到抵押信息'));
-          }
-
-          final cowNum = mortgageInfo.cowNum ?? 0;
-          final pastureNum = mortgageInfo.pastureNum ?? 0;
-          
-          return Container(
-            padding: const EdgeInsets.all(16.0),
-            // decoration: BoxDecoration(
-            //   gradient: LinearGradient(
-            //     colors: [Colors.blue[50]!, Colors.blue[100]!],
-            //     begin: Alignment.topCenter,
-            //     end: Alignment.bottomCenter,
-            //   ),
-            // ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                _buildInfoCard(
-                  "总牛只数量",
-                  cowNum.toString(),
-                  "assets/icon/svg/optimized/niu.svg",
-                  bgColor: const Color(0xFFFC8731),
-                  iconColor: Colors.white,
-                ),
-                const SizedBox(height: 16.0),
-                _buildInfoCard(
-                  "牧场数量",
-                  pastureNum.toString(),
-                  "assets/icon/svg/optimized/muchang.svg",
-                  bgColor: const Color(0xFF00BC7A),
-                  iconColor: Colors.white,
-                )
-              ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            AreaSelector(
+              enableCitySelection: true,
+              // enableDistrictSelection: true,
+              onAreaSelected: _onAreaSelected,
             ),
-          );
-        },
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error: (error, stack) => Center(child: Text('Error: $error')),
+            const SizedBox(height: 16.0),
+            Expanded(
+              child: mortgageInfo.when(
+                data: (data) {
+                  if (data == null) {
+                    return const Center(child: Text('未获取到抵押信息'));
+                  }
+
+                  final cowNum = data.cowNum ?? 0;
+                  final pastureNum = data.pastureNum ?? 0;
+
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      _buildInfoCard(
+                        "总牛只数量",
+                        cowNum.toString(),
+                        "assets/icon/svg/optimized/niu.svg",
+                        bgColor: const Color(0xFFFC8731),
+                        iconColor: Colors.white,
+                      ),
+                      const SizedBox(height: 16.0),
+                      _buildInfoCard(
+                        "牧场数量",
+                        pastureNum.toString(),
+                        "assets/icon/svg/optimized/muchang.svg",
+                        bgColor: const Color(0xFF00BC7A),
+                        iconColor: Colors.white,
+                      )
+                    ],
+                  );
+                },
+                loading: () => const Center(child: CircularProgressIndicator()),
+                error: (error, stack) => Center(child: Text('Error: $error')),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
